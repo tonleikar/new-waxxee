e16eaf39267473c542288371a5dc44e8a2c4aca0
Sam Gorman-Hailey <sam.gormanhailey@pm.me>
2026-06-15 15:12:23 +0100

    add name to folders

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_06_09_154443) do
+ActiveRecord::Schema[8.1].define(version: 2026_06_15_140922) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -25,6 +25,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_06_09_154443) do
 
   create_table "folders", force: :cascade do |t|
     t.datetime "created_at", null: false
+    t.string "name", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_folders_on_user_id"
